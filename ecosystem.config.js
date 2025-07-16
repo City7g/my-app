9470dc0b25d80de6552d01b830865db035cba1ea
Dima Mironenko <City7gor@gmail.com>
2025-07-16 10:32:34 +0300

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-app",
+      script: "npm",
+      args: "run preview",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+    },
+  ],
+};
